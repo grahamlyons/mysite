10ad6fad661e718388a8fe7311c1f5056b976ea2
Graham Lyons <graham@grahamlyons.com>
2012-11-26 09:42:51 +0000

    Return 404 for unfound article

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, make_response
 import articles
 app = Flask(__name__)
 
@@ -13,6 +13,8 @@ def index():
 @app.route("/article/<url_code>")
 def article(url_code=None):
     article = articles.get_article(url_code)
+    if not article:
+        abort(404)
     return render_template('article.html', article=article)
 
 if __name__ == "__main__":
