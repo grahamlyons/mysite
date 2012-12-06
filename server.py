1ba43e77504ee5a5cfd18b8f1d7b8ef70f0117aa
Graham Lyons <graham@grahamlyons.com>
2012-12-06 17:32:43 +0000

    Restrict routes to GET, make string quoting consistent

--- server.py
+++ server.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 DIR='./articles'
 articles = articles.Articles(DIR)
 
-@app.route("/")
+@app.route('/', methods=['GET'])
 def index():
     a = articles.get_articles()
     response = make_response(render_template('index.html', articles=a))
     response.headers['Cache-Control'] = 'max-age=3600'
     return response
 
-@app.route("/article/<url_code>")
+@app.route('/article/<url_code>', methods=['GET'])
 def article(url_code=None):
     article = articles.get_article(url_code)
     if not article:
         abort(404)
     return render_template('article.html', article=article)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
