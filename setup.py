7877946691246d1792944f4d09ad699bfed48371
Graham Lyons <glyons@ebay.com>
2015-03-18 04:21:33 +0000

    Give some real details in setup

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
+setup(name='MySite',
+      version='0.1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      author='Graham Lyons',
+      author_email='graham@grahamlyons.com',
+      url='http://grahamlyons.com'
      )
