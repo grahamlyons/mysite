db8ec57dde4ef78999724678ac572f91df78746f
Graham Lyons <glyons@ebay.com>
2015-03-18 04:06:26 +0000

    Add middleware and rename app.py to work on openshift

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-import os
+from os import environ
 
-from app import app as application
+from mysite import app as application
 
 #
 # Below for testing only
 #
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
+    port = environ.get('OPENSHIFT_PYTHON_PORT', 8051)
+    ip = environ.get('OPENSHIFT_PYTHON_IP', '0.0.0.0')
+    httpd = make_server('localhost', ip, application)
     # Wait for a single request, serve it and quit.
     httpd.handle_request()
